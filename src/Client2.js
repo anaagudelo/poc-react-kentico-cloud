771763ce63d7377fe2099af98f5c6124dd46501a
Ana Maria Agudelo <anaagudelo@seven4n.com>
2019-05-13 14:43:22 -0500

    Initial commit

--- src/Client2.js
+++ src/Client2.js
@@ -4,7 +4,7 @@ import { defaultProjectId } from "./Utilities/configProjectId";
 import {Home} from './Models/Home'
 
 const projectId = defaultProjectId;
-const previewApiKey = "";
+const previewApiKey = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIyOGZjY2ViNmY5M2Q0YTNhOTllNGEwNjY5MjA5YzQ1MSIsImlhdCI6IjE1NTYwMjIxOTMiLCJleHAiOiIxOTAxNjIyMTkzIiwicHJvamVjdF9pZCI6IjM3OWE1NzBhNjRiNzAwMDNhZmIwMGUzYzMyZTk0MDQ0IiwidmVyIjoiMS4wLjAiLCJhdWQiOiJwcmV2aWV3LmRlbGl2ZXIua2VudGljb2Nsb3VkLmNvbSJ9._hxnysTB2wb5gzNgzlITSzqpyHWW4E8ZfItjmzHRNQs";
 
 // configure type resolvers
 let typeResolvers = [
